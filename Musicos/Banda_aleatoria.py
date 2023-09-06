dec0a5dbd85f72d1c475dbd0f2ae593e0318012c
Jonathan5687 <143183321+Jonathan5687@users.noreply.github.com>
2023-09-05 23:40:31 -0500

    Banda_aleatoria.py

--- Musicos/Banda_aleatoria.py
+++ Musicos/Banda_aleatoria.py
@@ -8,6 +8,6 @@ t.agregar_instrumento(caja)
 t.agregar_instrumento(acordeon)
 t.agregar_instrumento(bajo)
 
-for i in range(5): 
+for i in range(3): 
     a = t.entregar_instrumento()
     print(a.afinar(), a.tocar())
